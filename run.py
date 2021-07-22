5a8c2a93ad918eae2fd29ba0ef758d7dc875d82e
SXKDZ <mr.sxkdz@gmail.com>
2021-07-22 12:13:40 +0800

    Update arXiv API and support multiple recipients

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
 import os
 import yaml
 import arxiv
-import textwrap
 import smtplib, ssl
 
-from time import sleep
 from pathlib import Path
-from datetime import date, datetime
+from datetime import date
 
 
 today = date.today()
@@ -22,38 +20,39 @@ def build_query(domains, keyword):
     return query
 
 
-def build_content(config):
-    domains = config['domains']
-    keywords = config['keywords']
-    query_config = config['query_config']
+def build_content(query, query_config):
+    domains = query['domains']
+    keywords = query['keywords']
+    total_mail = len(query['keywords'])
 
-    total_mail = len(config['keywords'])
     subject_placeholder = 'arXiv newsletter ' + str(today) + ' {index}/' + str(total_mail)
     content_placeholder = '\n' + '*' * 35 + '\n ' + subject_placeholder + ' \n' + '*' * 35 + '\n'
-    entry_placeholder = '{index}. {title}\n{authors}\nPublished at: {publish}\nUpdated at: {update}\nCategories: {categories}\n{notes}\n{link}\n\nAbstract:\n{abstract}\n'
+    entry_placeholder = '{index}. {title}\n{authors}\nPublished at: {publish}\nUpdated at: {update}\nPrimary Category: {primary_category}\nCategories: {categories}\n{notes}\n{link}\n\nAbstract:\n{abstract}\n'
 
     messages = []
 
     for i, keyword in enumerate(keywords):
         query = build_query(domains, keyword)
+        print(query)
         while True:
             try:
-                results = arxiv.query(query=query, **query_config)
+                results = arxiv.Search(query=query, **query_config)
                 break
             except:
                 pass
         entries = ''
-        for j, result in enumerate(results):
+        for j, result in enumerate(results.results()):
             entry = entry_placeholder.format(
                 index=j + 1,
                 title=result.title,
-                authors=', '.join(result.authors),
+                authors=', '.join([author.name for author in result.authors]),
                 publish=result.published,
                 update=result.updated,
-                categories=', '.join([tag.term for tag in result.tags]),
-                link=result.arxiv_url,
+                primary_category=result.primary_category,
+                categories=', '.join(result.categories),
+                link='\n'.join([link.href for link in result.links]),
                 abstract=result.summary,
-                notes=f'Comments: {result.arxiv_comment}\n' if result.arxiv_comment is not None else ''
+                notes=f'Comments: {result.comment}\n' if result.comment is not None else ''
             )
             entries += entry + '\n'
         subject = subject_placeholder.format(index=i + 1)
@@ -72,23 +71,28 @@ def send_mail(config):
     port = mail_config['port']
     sender = mail_config['user']
     password = mail_config['password']
+
+    query_config = config['query_config']
+    query_config['sort_by'] = arxiv.SortCriterion._member_map_[query_config['sort_by']]
+    query_config['sort_order'] = arxiv.SortOrder._member_map_[query_config['sort_order']]
+
     context = ssl.create_default_context()
     comments = '\nPowered by arXiv-newsletter \nhttps://github.com/SXKDZ/arXiv-newsletter\n'
     
-    messages = build_content(config)
-    for recipient in mail_config['recipient']:
+    for recipient in config['recipients']:
+        messages = build_content(query=recipient, query_config=query_config)
         for i, each_message in enumerate(messages):
             subject, content = each_message
 
-            header = f'To: {recipient}\nFrom: arXiv-bot <{sender}>\nSubject: {subject}\n'
+            header = f'To: {recipient["recipient"]}\nFrom: arXiv-bot <{sender}>\nSubject: {subject}\n'
             message = header + content + comments
             print(header)
-            with open(f'{today}/{i + 1}.txt', 'w+') as f:
+            with open(f'{today}/{recipient["recipient"]}_{i + 1}.txt', 'w+') as f:
                 f.write(message)
             try:
                 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
                     server.login(sender, password)
-                    server.sendmail(sender, recipient, message.encode('ascii', 'ignore').decode('ascii'))
+                    server.sendmail(sender, recipient['recipient'], message.encode('ascii', 'ignore').decode('ascii'))
             except smtplib.SMTPDataError as e:
                 print(e.smtp_error)
                 print()
